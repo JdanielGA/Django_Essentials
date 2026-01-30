4330ee4e98514dd990e53d9605f8f8d1814f7b9c
JDanielG <j.daniel_granada@hotmail.com>
2026-01-30 13:23:31 -0500

    First Model and Superuser

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,5 @@
+# tasks/admin.py
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+admin.site.register(Task)
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,12 @@
+# tasks/models.py
 from django.db import models
 
-# Create your models here.
+# Task model.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
