3490e8bdbca0e14727d53ab8a6a4bc1315c40ab9
JDanielG <j.daniel_granada@hotmail.com>
2026-01-30 21:26:48 -0500

    CRUD implementation of R

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,15 @@
+# tasks/views.py
 from django.shortcuts import render
+from .models import Task
 
-# Create your views here.
+# Lists view to display all tasks.
+def task_list(request):
+    tasks = Task.objects.all().order_by('-created_at')
+    context = {'tasks': tasks}
+    return render(request, 'tasks/task_list.html', context)
+
+# Detail Task View.
+def task_detail(request, task_id):
+    task = Task.objects.get(id=task_id)
+    context = {'task': task}
+    return render(request, 'tasks/task_details.html', context)
