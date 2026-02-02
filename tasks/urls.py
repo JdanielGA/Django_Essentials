7225a8587e106659100be0513c7cbfaf7206d91b
JDanielG <j.daniel_granada@hotmail.com>
2026-02-02 10:39:47 -0500

    The update function was added

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('<int:task_id>/', views.task_detail, name='task_details'),
     path('new/', views.task_form, name='task_form'),
+    path('<int:task_id>/edit/', views.task_update, name='task_update'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,5 @@
 # tasks/views.py
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 from .forms import TaskForm
 
@@ -15,7 +15,7 @@ def task_detail(request, task_id):
     context = {'task': task}
     return render(request, 'tasks/task_details.html', context)
 
-# Form View for creating and updating tasks.
+# Form View for creating tasks.
 def task_form(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
@@ -26,3 +26,16 @@ def task_form(request):
         form = TaskForm()
     context = {'form': form}
     return render(request, 'tasks/task_form.html', context)
+
+# Form View for updating tasks.
+def task_update(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    context = {'form': form, 'task': task}
+    return render(request, 'tasks/update_task.html', context)
